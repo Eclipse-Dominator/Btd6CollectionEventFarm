f499144381c54a477f84d5654d30217a2ed75e1a
ZQ <li.zhaoqi@dhs.sg>
2024-05-14 09:19:02 +0800

    Refocuses window when action is required

--- stage_player.py
+++ stage_player.py
@@ -3,25 +3,26 @@ Plays, complete and exit the stage to main menu
 '''
 
 from keylog_replay import make_instruction, read_keylog, run_instruction, make_setting
-from utils import locate_by_white, find_img, wait_til_exists, focus_game, sleep, r2a, g_x, g_y, g_w, g_h
+from utils import click, locate_by_white, find_img, wait_til_exists, focus_game, sleep, r2a, g_x, g_y, g_w, g_h
 from datetime import datetime
 from assets import success_next_icon, success_home_icon, collect_icon, insta_icon, reward_continue_icon, back_icon, defeat_icon, level_up_icon, restart_icon, restart_btn_icon, overwrite_ok_icon, stage_loaded_check_icon
-import pyautogui
 from pynput.keyboard import Key, Controller
 
 def restart_stage():
   keyboard = Controller()
+  focus_game()
   while not(pos := find_img(restart_btn_icon)):
     keyboard.press(Key.esc)
     keyboard.release(Key.esc)
-    sleep(1)
+    sleep(.6)
   
-  pyautogui.click(r2a(pos))
-  pyautogui.click(r2a(wait_til_exists(restart_icon)))
+  click(r2a(pos))
+  click(r2a(wait_til_exists(restart_icon)))
   wait_til_exists(stage_loaded_check_icon)
 
 def resume_stage():
   keyboard = Controller()
+  focus_game()
   keyboard.press(Key.esc)
   keyboard.release(Key.esc)
   sleep(1)
@@ -54,24 +55,25 @@ def check_completion():
   return False
 
 def return_to_home():
-  pyautogui.click(r2a(wait_til_exists(success_next_icon)))
-  pyautogui.click(r2a(wait_til_exists(success_home_icon)))
+  focus_game()
+  click(r2a(wait_til_exists(success_next_icon)))
+  click(r2a(wait_til_exists(success_home_icon)))
   sleep(5) # leave 5 sec buffer for collection of instas to appear
   if pos := find_img(collect_icon):
     collect_instas(pos)
 
 def collect_instas(continue_pos):
-  pyautogui.click(r2a(continue_pos))
+  click(r2a(continue_pos))
   wait_til_exists(insta_icon, mode_bw=True) # wait for insta icon to appear
   print("Collecting instas")
   while pos:=locate_by_white(insta_icon):
-    pyautogui.click(r2a(pos))
+    click(r2a(pos))
     sleep(1)
-    pyautogui.click(r2a(pos))
+    click(r2a(pos))
     sleep(1)
   
-  pyautogui.click(r2a(wait_til_exists(reward_continue_icon)))
-  pyautogui.click(r2a(wait_til_exists(back_icon)))
+  click(r2a(wait_til_exists(reward_continue_icon)))
+  click(r2a(wait_til_exists(back_icon)))
 
 if __name__ == "__main__":
   run_stage("INFERNAL")
